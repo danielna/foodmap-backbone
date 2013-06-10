404fab1a12263ab5d3c3e99ab1eff29db1ebe132
Daniel Na <daniel.na@gmail.com>
2013-06-10 17:03:18 -0400

    cleanup

--- assets/js/views/ListingView.js
+++ assets/js/views/ListingView.js
@@ -8,8 +8,6 @@ foodmap.ListingView = Backbone.View.extend({
 
     template: _.template( $("#template-listing").html() ),
 
-    // reset
-    // load all listings
     render: function() {
         this.model.attributes.price_map = foodmap._globals.price_map[this.model.get("price")];
         this.$el.html( this.template( this.model.toJSON() ));
