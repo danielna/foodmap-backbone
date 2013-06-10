d3348745fffbd1b69bce4a4473ae9807f79934a6
Daniel Na <daniel.na@gmail.com>
2013-06-10 13:19:51 -0400

    Add click events to buttons in header, relating to hide/showing the welcome message

--- assets/js/views/foodmap.main.js
+++ assets/js/views/foodmap.main.js
@@ -3,13 +3,37 @@ var foodmap = foodmap || {};
 
 foodmap.Main = Backbone.View.extend({
 
+    el: "body",
+
+    events: {
+        "click #js-btn-welcome": "toggleWelcome",
+        "click #js-close-welcome": "toggleWelcome",
+        "click #js-btn-reset": "onReset"
+    },
+    
     initialize: function() {
+        this.$container_welcome = this.$("#container-welcome");
+        this.$tags = this.$(".tags .tag");
+
         foodmap.MapList = new foodmap.MapItemList();
         var 
             listingContainerView = new foodmap.ListingContainerView({ collection: foodmap.MapList }),
             tagsView = new foodmap.TagsView({ collection: foodmap.MapList });        
 
         foodmap.MapList.fetch({reset: true});       
+    },
+
+    toggleWelcome: function() {
+        this.$container_welcome.fadeToggle();
+    },
+
+    resetActiveTag: function() {
+        this.$tags.removeClass("active");
+    },
+
+    onReset: function() {
+        this.$container_welcome.fadeIn();
+        this.resetActiveTag();
     }
 
 });
