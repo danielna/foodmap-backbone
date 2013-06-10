a6ae7a41504bec7a1720e7063df596714e59e57e
Daniel Na <daniel.na@gmail.com>
2013-06-10 13:20:15 -0400

    Add a shallow click event to the tags to add class "active"

--- assets/js/views/foodmap.TagsView.js
+++ assets/js/views/foodmap.TagsView.js
@@ -7,14 +7,27 @@ foodmap.TagsView = Backbone.View.extend({
 
     template: _.template( $("#template-tag").html() ),
 
+    events: {
+        "click .tag": "clickTag"
+    },
+
     initialize: function() {
+        this.$container_tags = this.$("#container-tags");
+        this.$container_ethnicities = this.$("#container-ethnicities");
+
         this.collection.bind("reset", _.bind(this.render, this));
     },
 
     render: function() {
-        this.$("#container-tags").html( this.template({ tags: this.collection.tags() }) );
-        this.$("#container-ethnicities").html( this.template({ tags: this.collection.ethnicities() }) );
-    }
+        this.$container_tags.html( this.template({ tags: this.collection.tags() }) );
+        this.$container_ethnicities.html( this.template({ tags: this.collection.ethnicities() }) );
+        this.$tags = this.$(".tag");
+    },
 
+    clickTag: function(event) {
+        var $this = $(event.currentTarget);
+        this.$tags.removeClass("active");
+        $this.addClass("active");
+    }
 
 });
